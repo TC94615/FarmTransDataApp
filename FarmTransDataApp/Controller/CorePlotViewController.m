f3f192fcc76436359c91de3ba3c431afa9350439
TC94615 <TCLee0117@gmail.com>
2015-01-16 14:46:09 +0800

    Multi-plot

--- FarmTransDataApp/Controller/CorePlotViewController.m
+++ FarmTransDataApp/Controller/CorePlotViewController.m
@@ -12,6 +12,18 @@ struct MinMaxValue {
     float max;
 };
 
+enum {
+    TopPlot = 0,
+    MidPlot,
+    BotPlot,
+    AvgPlot
+};
+
+NSString *const TOP_PRICE_PLOT_IDENTIFIER = @"topPricePlotIdentifier";
+NSString *const MID_PRICE_PLOT_IDENTIFIER = @"midPricePlotIdentifier";
+NSString *const BOT_PRICE_PLOT_IDENTIFIER = @"botPricePlotIdentifier";
+NSString *const AVG_PRICE_PLOT_IDENTIFIER = @"avgPricePlotIdentifier";
+
 @interface CorePlotViewController()<CPTPlotDataSource>
 @property (nonatomic, strong) NSArray *dataForPlot;
 @end
@@ -28,10 +40,23 @@ struct MinMaxValue {
 
 - (NSNumber *) numberForPlot:(CPTPlot *) plot field:(NSUInteger) fieldEnum recordIndex:(NSUInteger) idx {
     NSString *key = (fieldEnum == CPTScatterPlotFieldX ? @"x" : @"y");
-    NSNumber *num = [self.dataForPlot[idx] topPrice];
+    NSNumber *num;
+    num = [self.dataForPlot[idx] topPrice];
+    if ([(NSString *) plot.identifier isEqualToString:TOP_PRICE_PLOT_IDENTIFIER]) {
+        num = [self.dataForPlot[idx] topPrice];
+    }
+    else if ([(NSString *) plot.identifier isEqualToString:MID_PRICE_PLOT_IDENTIFIER]) {
+        num = [self.dataForPlot[idx] midPrice];
+    }
+    else if ([(NSString *) plot.identifier isEqualToString:BOT_PRICE_PLOT_IDENTIFIER]) {
+        num = [self.dataForPlot[idx] botPrice];
+    }
+    else if ([(NSString *) plot.identifier isEqualToString:AVG_PRICE_PLOT_IDENTIFIER]) {
+        num = [self.dataForPlot[idx] avgPrice];
+    }
 
     if (fieldEnum == CPTScatterPlotFieldY) {
-        NSLog(@">>>>>>>>>>>> x,y = (%u,%@)", idx, num);
+        NSLog(@">>>>>>>>>>>> plot:(x,y) = %@:(%u,%@)", plot.identifier, idx, num);
         return num;
     }
     return @(idx);
@@ -47,9 +72,15 @@ struct MinMaxValue {
 
 - (void) viewDidLoad {
     [super viewDidLoad];
-    CGRect frame250 = CGRectMake(0, 0, 250, 450);
+
     CPTTheme *theme = [CPTTheme themeNamed:kCPTDarkGradientTheme];
-    CPTGraphHostingView *hostView = [[CPTGraphHostingView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+
+    CGPoint viewOrigin = [[UIScreen mainScreen] bounds].origin;
+    CGSize viewSize = CGSizeMake([[UIScreen mainScreen] bounds].size.width, [[UIScreen mainScreen] bounds].size.height - 3 * self.navigationController.topLayoutGuide.length);
+    CGRect frame = CGRectMake(viewOrigin.x, viewOrigin.y, viewSize.width, viewSize.height);
+
+    CPTGraphHostingView *hostView = [[CPTGraphHostingView alloc] initWithFrame:frame];
+//    CPTGraphHostingView *hostView = [[CPTGraphHostingView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     hostView.backgroundColor = [UIColor blueColor];
     [self.view addSubview:hostView];
 
@@ -58,6 +89,16 @@ struct MinMaxValue {
     [graph applyTheme:theme];
 
     CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *) graph.defaultPlotSpace;
+
+    graph.paddingTop = 30.0;
+    graph.paddingBottom = 30.0;
+    graph.paddingLeft = 30.0;
+    graph.paddingRight = 30.0;
+
+    graph.plotAreaFrame.paddingTop = 20;
+    graph.plotAreaFrame.paddingBottom = 80;
+    graph.plotAreaFrame.paddingLeft = 20;
+    graph.plotAreaFrame.paddingRight = 20;
 
     NSMutableArray *transDateArray = [NSMutableArray array];
     NSMutableArray *topPriceArray = [NSMutableArray array];
@@ -80,15 +121,32 @@ struct MinMaxValue {
     [mixPriceArray addObjectsFromArray:botPriceArray];
     [mixPriceArray addObjectsFromArray:avgPriceArray];
 //    [mixPriceArray addObjectsFromArray:volumeArray];
+
     struct MinMaxValue minMaxValue = [self minMaxNumberInArray:mixPriceArray];
-    NSLog(@">>>>>>>>>>>> min,max = %f,%f", minMaxValue.min,minMaxValue.max);
-    [plotSpace setYRange:[CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(minMaxValue.min) length:CPTDecimalFromFloat(minMaxValue.max-minMaxValue.min)]];
+    NSLog(@">>>>>>>>>>>> min,max = %f,%f", minMaxValue.min, minMaxValue.max);
+    [plotSpace setYRange:[CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(minMaxValue.min)
+                                                      length:CPTDecimalFromFloat(minMaxValue.max - minMaxValue.min)]];
     [plotSpace setXRange:[CPTPlotRange plotRangeWithLocation:CPTDecimalFromFloat(-4) length:CPTDecimalFromFloat(8)]];
 
-    CPTScatterPlot *plot = [[CPTScatterPlot alloc] initWithFrame:CGRectZero];
-    plot.dataSource = self;
+    CPTScatterPlot *topPricePlot = [[CPTScatterPlot alloc] initWithFrame:CGRectZero];
+    topPricePlot.dataSource = self;
+    topPricePlot.identifier = TOP_PRICE_PLOT_IDENTIFIER;
+    [graph addPlot:topPricePlot toPlotSpace:graph.defaultPlotSpace];
 
-    [graph addPlot:plot toPlotSpace:graph.defaultPlotSpace];
+    CPTScatterPlot *midPricePlot = [[CPTScatterPlot alloc] initWithFrame:CGRectZero];
+    midPricePlot.dataSource = self;
+    midPricePlot.identifier = MID_PRICE_PLOT_IDENTIFIER;
+    [graph addPlot:midPricePlot toPlotSpace:graph.defaultPlotSpace];
+
+    CPTScatterPlot *botPricePlot = [[CPTScatterPlot alloc] initWithFrame:CGRectZero];
+    botPricePlot.dataSource = self;
+    botPricePlot.identifier = BOT_PRICE_PLOT_IDENTIFIER;
+    [graph addPlot:botPricePlot toPlotSpace:graph.defaultPlotSpace];
+
+    CPTScatterPlot *avgPricePlot = [[CPTScatterPlot alloc] initWithFrame:CGRectZero];
+    avgPricePlot.dataSource = self;
+    avgPricePlot.identifier = AVG_PRICE_PLOT_IDENTIFIER;
+    [graph addPlot:avgPricePlot toPlotSpace:graph.defaultPlotSpace];
 }
 
 
